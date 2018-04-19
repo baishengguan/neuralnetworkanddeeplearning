17571a30c6b58b990162855cd7586d0ae609877c
baisheng <15629572+baishengguan@users.noreply.github.com>
2018-04-19 11:03:17 +0800

    git

---
+++ test.py
@@ -0,0 +1,11 @@
+# ----------------------
+# - read the input data:
+
+import mnist_loader
+import network
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+training_data = list(training_data)
+
+net = network.Network([784, 30, 10])
+net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
